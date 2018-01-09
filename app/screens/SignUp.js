797beef396d70873d47cdc044ea2b4472b3676da
Amrita Srivastava <email.amritas@gmail.com>
2018-01-08 20:19:19 -0800

    first version: navigate

--- app/screens/SignUp.js
+++ app/screens/SignUp.js
@@ -1,32 +1,30 @@
 import React from "react";
-import { View } from "react-native";
-import { Card, Button, FormLabel, FormInput } from "react-native-elements";
+import { View, Image } from "react-native";
+import { Card, Button, FormLabel, FormInput, SocialIcon } from "react-native-elements";
 import { onSignIn } from "../auth";
 
 export default ({ navigation }) => (
   <View style={{ paddingVertical: 20 }}>
-    <Card>
-      <FormLabel>Email</FormLabel>
-      <FormInput placeholder="Email address..." />
-      <FormLabel>Password</FormLabel>
-      <FormInput secureTextEntry placeholder="Password..." />
-      <FormLabel>Confirm Password</FormLabel>
-      <FormInput secureTextEntry placeholder="Confirm Password..." />
-
-      <Button
-        buttonStyle={{ marginTop: 20 }}
-        backgroundColor="#03A9F4"
-        title="SIGN UP"
+    <Card
+      image = {require('../../assets/san_francisco.jpg')}>
+      
+      <SocialIcon
+        title='Sign In With Facebook'
+        button
+        type='facebook'
         onPress={() => {
           onSignIn().then(() => navigation.navigate("SignedIn"));
         }}
       />
-      <Button
-        buttonStyle={{ marginTop: 20 }}
-        backgroundColor="transparent"
-        textStyle={{ color: "#bcbec1" }}
-        title="Sign In"
-        onPress={() => navigation.navigate("SignIn")}
+      
+      <SocialIcon
+        title='Sign In With Goole'
+        button
+        type='google-plus-official'
+        onPress={() => {
+          onSignIn().then(() => navigation.navigate("SignedIn"));
+        }}
+        raised={true}
       />
     </Card>
   </View>
